305c1c59bc0abf1d438493bd776277868aad6956
logicscienc <anjuukumari001@gmail.com>
2025-11-25 19:40:19 +0530

    new commit

--- JS_Basic_3/index.js
+++ JS_Basic_3/index.js
@@ -49,7 +49,7 @@ console.log(date3);
 
 // Array Object : in javaScript the array is used to store multiple values in a single variable. if it is a interger, float, bool or string doesnt matter.
 
-let numbers = [1,2,3,4,5];
+// let numbers = [1,2,3,4,5];
 
 // below are some of ways using which we can insert values in our existing array.
 numbers.push(9);  //add 9 at the end of the array
@@ -65,7 +65,7 @@ console.log(numbers);
 numbers.includes(9); //return true
 
 
-// Array using object
+// Array of object
 
 let courses = [
     {no:1, naam: 'Love'},
@@ -76,7 +76,58 @@ console.log(courses);
 
 // we can check if a specific object is present in the array or not using some method.
 
-console.log(courses.includes({no:1, naam: 'Love'})); 
+console.log(courses.includes({no:1, naam: 'Love'})); //return false because it checks the reference not the content.
+
+// Callback function: A callback function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action. and one most imp. thing is parent of call back function is find method of array.
+
+let course = courses.find(function(course) {
+    return course.naam == 'Luv';
+})
+
+console.log(course);
+
+// Arrow function: 
+
+
+// Re,oving elements from an array
+
+// let numbers = [1,2,3,4,5,6,7];
+
+numbers.pop();  // use to remove elemenst from the end.
+numbers.shift(); // use to remove elements from the start.
+numbers.splice(2, 1); // this is used to remoev any perticular element from the array.like right now 3 will get remove. 
+
+
+console.log(numbers);
+
+
+// coping the address of an array
+
+let numbers = [1,2,3,4,5];
+let numbers2 = numbers; // so here the array of numbers string get into the numbers2 array by refrence.
+
+// now let's try to empty the array
+numbers.splice(0,numbers.length);
+
+console.log(numbers);
+console.log(numbers2);
+
+
+// combining two array
+
+let first = [1,2,3];
+let second = [4,5,6];
+
+let combined = first.concat(second);
+console.log(combined);
+
+
+// slice method
+
+let sliced = combined.slice(2,3);
+console.log(sliced);
+
+
 
 
 
