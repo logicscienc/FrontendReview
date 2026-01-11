176d239603c98644641b231d971d600dcee6b40d
logicscienc <anjuukumari001@gmail.com>
2026-01-11 23:09:51 +0530

    new commit

--- JS_Advance_1/index.js
+++ JS_Advance_1/index.js
@@ -18,15 +18,24 @@ let codingEx = document.querySelector('.code-example');
 // now let's try to print codingEx
 codingEx;
 
-// now let's see the HTML content of this element.
+// now let's see the HTML content of this element. 
+// innerHTML: whenever we use innerHTML property after adding any new tag inside the text then it will render that tag .
 
 codingEx.innerHTML
 
-// now as i told above we can even set the HTML of the element. so let's try to empty the HTML.
+// now as i told above we can even set the HTML of the element. so let's try to empty the HTML. 
 
 codingEx.innerHTML = ''   //
 
 
-codingEx.textContent   // the output will be like this 'Toggles <h1> Dark </h1>' this basicely shows the content which is inside that element whose class name is code-example.
+codingEx.textContent   // the output will be like this 'Toggles <h1> Dark </h1>' this basicely shows the content which is inside that element whose class name is code-example. if we want to get the text only without the HTML tags then we can use textContent property. whenever we use innerHTML property after adding any new tag inside the text then it will treat it as text .
 
+
+codingEx.innerText    // this will also give the same output as textContent. but the difference is that innerText will consider the styling of the element. for example if we have display none property in css then innerText will not fetch that text but textContent will fetch that text.
+
+
+
+// ***********************Adding new element/context *****************************
+
+// if we want to add any new tag in the previous existing code then we can use 
 
